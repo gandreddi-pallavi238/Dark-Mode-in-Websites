d1c0e625faccb32b8a7bf5a8ca0e9dc4ea501038
gandreddi-pallavi238 <214619468+gandreddi-pallavi238@users.noreply.github.com>
2025-12-30 18:49:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️ Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggleBtn.textContent = "🌙 Dark Mode";
+    }
+});
